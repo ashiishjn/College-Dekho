869eea6b51d54231ff097570cf4e9726fb5b310a
ASHISH JAIN <ashishjain4365@gmail.com>
2022-02-20 14:01:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const btnNavEl = document.querySelector(".search-college");
 const headerEl = document.querySelector(".search-js");
-const headEl = document.querySelector("body");
-btnNavEl.addEventListener("mouseover", function () {
-  headerEl.classList.add("search-list");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("search-list");
 });
-btnNavEl.addEventListener("mouseout", function () {
-  headerEl.classList.remove("search-list");
-});
-// headEl.addEventListener("click", function () {
-//   headerEl.classList.remove("search-list");
-// });
